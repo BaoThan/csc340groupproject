9bd9a0f3ade466bcde2d9537d7d26ab6c0704e40
BaoThan <109579681+BaoThan@users.noreply.github.com>
2023-05-20 22:31:22 -0700

    Fix missing strptime in Windows

--- product/product.cpp
+++ product/product.cpp
@@ -8,6 +8,19 @@
 using namespace std;
 
 
+#ifdef _WIN32
+/** @brief strptime implementation for windows. */
+extern "C" char* strptime(const char* s, const char* f, struct tm* tm) {
+  std::istringstream input(s);
+  input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+  input >> std::get_time(tm, f);
+  if (input.fail()) {
+    return nullptr;
+  }
+  return (char*)(s + input.tellg());
+}
+#endif
+
 bool Product::isExpired()
 {
     struct tm _;
